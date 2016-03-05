6d9764caf3993dc78abb7b106193975fea84de7b
Control Zeta <francisco.arroyo@controlzeta.com.mx>
2016-03-05 12:13:47 -0600

    Owl-Carousel y SEO-Analytics

--- js/init.js
+++ js/init.js
@@ -42,6 +42,18 @@
         return false;
     });
 
+    var owl = $("#owl-social-media");
+
+      owl.owlCarousel({
+      items : 1, //10 items above 1000px browser width
+      itemsDesktop : [1000,5], //5 items between 1000px and 901px
+      itemsDesktopSmall : [900,3], // betweem 900px and 601px
+      itemsTablet: [600,2], //2 items between 600 and 0
+      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+  });
+
+  owl.trigger('owl.play',1000);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
@@ -87,3 +99,4 @@ var radarChartData = {
             responsive: true
         });
     }
+

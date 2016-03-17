b6f3c4f01ed9a6aa0ba398949e9bf1c115e8b9e9
Control Zeta <francisco.arroyo@controlzeta.com.mx>
2016-03-16 18:58:30 -0600

    Cambios en Correo, compresión de imagenes y robots

--- js/init.js
+++ js/init.js
@@ -54,6 +54,15 @@
 
   owl.trigger('owl.play',3000);
 
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-75221901-1', 'auto');
+  ga('send', 'pageview');
+
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
@@ -131,3 +140,53 @@ var linedata = {
             bezierCurve: true
         });
     }
+
+$("#form1").submit(function(event) {
+     var ajaxRequest;
+
+    /* Stop form from submitting normally */
+    event.preventDefault();
+
+    /* Clear result div*/
+    $("#result").html('');
+
+    if($('#comments').val() == "" || $('#subject').val() == ""  || $('#first_name').val() == ""  || $('#email').val() == "" )
+    {
+        $("#result").html('Completa los datos que faltan para enviar el correo');
+        return false;
+    }
+
+    /* Get from elements values */
+    var values = { //Fetch form data
+            'comments'     : $('#comments').val(),
+            'subject'     : $('#subject').val(),
+            'first_name'     : $('#first_name').val(),
+            'last_name'     : $('#last_name').val(),
+            'email'     : $('#email').val(),
+        };
+
+    /* Send the data using post and put the results in a div */
+    /* I am not aborting previous request because It's an asynchronous request, meaning 
+       Once it's sent it's out there. but in case you want to abort it  you can do it by  
+       abort(). jQuery Ajax methods return an XMLHttpRequest object, so you can just use abort(). */
+       ajaxRequest= $.ajax({
+            url: "sendMail.php",
+            type: "POST",
+            data: values
+        });
+
+      /*  request cab be abort by ajaxRequest.abort() */
+
+     ajaxRequest.done(function (response, textStatus, jqXHR){
+          // show successfully for submit message
+          $("#result").html('Gracias por contactarnos');
+     });
+
+     /* On failure of request this function will be called  */
+     ajaxRequest.fail(function (){
+
+       // show error
+       $("#result").html('Ha ocurrido un error por favor inténtalo más tarde');
+     });
+   });
+   
--- sendMail.php
+++ sendMail.php
@@ -1,9 +1,9 @@
 
 <?php 
 
-$destinatario = "francisco.arroyo@x1mexico.com";
+$destinatario = "francisco.arroyo@controlzeta.com.mx";
 $asunto = "Contacto Mezzopane"; 
-$cuerpo = '<html><head><title>Mensaje desde Mezopanne.com</title></head><body><h1>Este correo llego a traves de mezzopane.com</h1>';
+$cuerpo = '<html><head><title>Mensaje desde Mezopanne.com</title></head><body><h1>Este correo llego a traves de CV</h1>';
 $cuerpo .= "<p>De: " . $_POST['email'] . " " . $_POST['first_name'] . " " . $_POST['last_name'] . "</p>";
 $cuerpo .= "<p>Asunto: " . $_POST['subject'] . " </p>";
 $cuerpo .= "<p>Comentarios: " . $_POST['comments'] . "</p>";
@@ -22,13 +22,13 @@ $headers .= "From: Web Mezzopane <" . $_POST['email'] . ">\r\n";
 $headers .= "Reply-To: " . $_POST['email'] . " \r\n"; 
 
 //ruta del mensaje desde origen a destino 
-$headers .= "Return-path: contacto@mezzopane.com \r\n"; 
+$headers .= "Return-path: francisco.arroyo@controlzeta.com.mx \r\n"; 
 
 //direcciones que recibián copia 
-$headers .= "Cc: francisco.arroyo@x1mexico.com\r\n"; 
+$headers .= "Cc: francisco.arroyo@x1mexico.com \r\n"; 
 
 //direcciones que recibirán copia oculta 
-$headers .= "Bcc: francisco.arroyo@x1mexico.com\r\n"; 
+$headers .= "Bcc: francisco.arroyo@x1mexico.com \r\n"; 
 
 mail($destinatario,$asunto,$cuerpo,$headers) 
 
